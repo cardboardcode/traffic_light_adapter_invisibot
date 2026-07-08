1009371641092067fbfb1a9901385342d87df013
Gary Bey <beyhy94@gmail.com>
2026-07-08 10:02:32 +0800

    :heavy_plus_sign: Added dev scripts for easier deployment and uploading to the repo registry for remote image download.
    Signed-off-by: Gary Bey <beyhy94@gmail.com>

---
+++ scripts/4_push_docker_image.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker tag traffic_light_adapter_invisibot:jazzy ghcr.io/cardboardcode/traffic_light_adapter_invisibot:jazzy
+docker push ghcr.io/cardboardcode/traffic_light_adapter_invisibot:jazzy
